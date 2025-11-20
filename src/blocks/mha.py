858bc16373ae4e47cc39ec494f1e411fee6578b3
ucinereo <nicola.studer2000@gmail.com>
2025-11-20 21:59:06 +0100

    [RoPE] Generalize RoPE to RelativePositionEncoding

--- src/blocks/mha.py
+++ src/blocks/mha.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 import torch
 import torch.nn as nn
@@ -6,7 +7,7 @@ from einops import rearrange
 from rotary_embedding_torch import RotaryEmbedding
 from torch import Tensor
 
-from src.interfaces import AttentionBase
+from src.interfaces import AttentionBase, BaseRelativePositionalEncoding
 from src.utils import get_forward_metadata
 
 logger = logging.getLogger(__name__)
@@ -23,6 +24,7 @@ class MultiHeadAttention(AttentionBase):
         q_dim: int = None,
         ctx_dim: int = None,
         dropout: float = 0.0,
+        relative_pos_encoding: Optional[BaseRelativePositionalEncoding] = None,
     ):
         """Initialize multi-head attention.
 
@@ -33,6 +35,7 @@ class MultiHeadAttention(AttentionBase):
             q_dim: Query dimensionality (defaults to dim).
             ctx_dim: Context dimensionality (defaults to dim).
             dropout: Dropout rate.
+            relative_pos_encoding: Relative positional encoding (defaults to None)
         """
         super().__init__()
 
@@ -50,8 +53,7 @@ class MultiHeadAttention(AttentionBase):
         self.output = torch.nn.Linear(dim, q_dim, bias=embed_bias)
 
         self.dropout = nn.Dropout(dropout)
-        self.rope_h = RotaryEmbedding(dim=dim_head // 2)
-        self.rope_w = RotaryEmbedding(dim=dim_head // 2)
+        self.relative_pos_encoding = relative_pos_encoding
 
     def forward(
         self,
@@ -59,18 +61,16 @@ class MultiHeadAttention(AttentionBase):
         ctx: Tensor,
         mask: torch.Tensor = None,
     ) -> tuple[Tensor, Tensor]:
-        orig_shape = get_forward_metadata("orig_shape")
-        _, h, w = orig_shape
-
         # Apply projection
         q, k, v = map(lambda f, x: f(x), (self.query, self.key, self.value), (q, ctx, ctx))
 
         # Split into 'n_heads' heads
         q, k, v = map(lambda x: rearrange(x, "b s (h d) -> b h s d", h=self.n_heads), (q, k, v))
 
-        # Apply 2D rotational encoding
-        q = self._apply_2d_rope(q, h, w)
-        k = self._apply_2d_rope(k, h, w)
+        # Apply relative positional encoding if possible
+        if self.relative_pos_encoding is not None:
+            print(f"rel pos enc type: {type(self.relative_pos_encoding)}")
+            q, k = self.relative_pos_encoding(q, k)
 
         # Compute q @ k^T / sqrt(d)
         scores = torch.einsum("b h s d, b h t d -> b h s t", q, k) * self.scale
@@ -93,44 +93,3 @@ class MultiHeadAttention(AttentionBase):
         out = self.dropout(out)
 
         return out, attn
-
-    def _apply_2d_rope(self, x: torch.Tensor, h: int, w: int) -> torch.Tensor:
-        """Apply 2D RoPE encoding to x. The features of x are being splitted into
-        x_h and x_w, and on those two different rotational encoding are being applied.
-        After the encoding, x_h and x_w are again merged into the correct shape.
-
-        Args:
-            x: (b h s d) input tokens (without registers)
-            h: height of the grid
-            w: width of the grid
-        """
-        b, n_heads, s, d_full = x.shape
-        d_half = d_full // 2
-
-        # Remove the head specific registers
-        n_registers = s - (h * w)
-        registers = x[..., :n_registers, :]
-        x = x[..., n_registers:, :]
-
-        x = rearrange(x, "b h (gh gw) d -> b h gh gw d", gh=h, gw=w)
-
-        # Split x in both dimensions
-        x_h = x[..., :d_half]
-        x_w = x[..., d_half:]
-
-        # Apply rope to height
-        x_h = rearrange(x_h, "b h gh gw d -> b (h gw) gh d")
-        x_h = self.rope_h.rotate_queries_or_keys(x_h)
-        x_h = rearrange(x_h, "b (h gw) gh d -> b h (gh gw) d", h=n_heads, gw=w)
-
-        # Apply rope to width
-        x_w = rearrange(x_w, "b h gh gw d -> b (h gh) gw d")
-        x_w = self.rope_h.rotate_queries_or_keys(x_w)
-        x_w = rearrange(x_w, "b (h gh) gw d -> b h (gh gw) d", h=n_heads, gh=h)
-
-        # Concatenate back together
-        x = torch.cat([x_h, x_w], dim=-1)
-
-        # Add registers back
-        x = torch.cat([registers, x], dim=2)
-        return x
---
+++ src/encodings/rope.py
@@ -0,0 +1,74 @@
+import torch
+from einops import rearrange
+from rotary_embedding_torch import RotaryEmbedding
+from torch import Size, Tensor
+
+from src.interfaces import BaseRelativePositionalEncoding
+from src.utils import get_forward_metadata
+
+
+class RotaryPositionalEncoding(BaseRelativePositionalEncoding):
+    """Rotary positional encoding (Su et al., 2021)"""
+
+    def __init__(self, dim: int, n_heads: int):
+        """Initialize rotary positional encoding.
+
+        Args:
+            dim: Token dimensionality.
+            n_heads: Number of attention heads.
+        """
+
+        # We apply the rotary embedding to each token in every head separately.
+        self.rope_h = RotaryEmbedding(dim=dim // n_heads)
+        self.rope_w = RotaryEmbedding(dim=dim // n_heads)
+
+    def forward(self, q: Tensor, k: Tensor) -> tuple[Tensor, Tensor]:
+        # Fetch the grid size
+        orig_shape = get_forward_metadata("orig_shape")
+        _, h, w = orig_shape
+
+        q = self._apply_2d_rope(q, h, w)
+        k = self._apply_2d_rope(k, h, w)
+
+        return (q, k)
+
+    def _apply_2d_rope(self, x: Tensor, h: int, w: int) -> Tensor:
+        """Apply 2D RoPE encoding to x. The features of x are being splitted into
+        x_h and x_w, and on those two different rotational encoding are being applied.
+        After the encoding, x_h and x_w are again merged into the correct shape.
+
+        Args:
+            x: (b h s d) input tokens (with potential registers)
+            h: height of the grid
+            w: width of the grid
+        """
+        b, n_heads, s, d_full = x.shape
+        d_half = d_full // 2
+
+        # Remove the head specific registers
+        n_registers = s - (h * w)
+        registers = x[..., :n_registers, :]
+        x = x[..., n_registers:, :]
+
+        x = rearrange(x, "b h (gh gw) d -> b h gh gw d", gh=h, gw=w)
+
+        # Split x in both dimensions
+        x_h = x[..., :d_half]
+        x_w = x[..., d_half:]
+
+        # Apply rope to height
+        x_h = rearrange(x_h, "b h gh gw d -> b (h gw) gh d")
+        x_h = self.rope_h.rotate_queries_or_keys(x_h)
+        x_h = rearrange(x_h, "b (h gw) gh d -> b h (gh gw) d", h=n_heads, gw=w)
+
+        # Apply rope to width
+        x_w = rearrange(x_w, "b h gh gw d -> b (h gh) gw d")
+        x_w = self.rope_h.rotate_queries_or_keys(x_w)
+        x_w = rearrange(x_w, "b (h gh) gw d -> b h (gh gw) d", h=n_heads, gh=h)
+
+        # Concatenate back together
+        x = torch.cat([x_h, x_w], dim=-1)
+
+        # Add registers back
+        x = torch.cat([registers, x], dim=2)
+        return x
